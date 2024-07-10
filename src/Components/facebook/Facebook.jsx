9381537791ebe16d6473c8ee3c2891ed1d0c9848
Roronoa Zoro <shivamsharma8434202092@gmail.com>
2024-07-10 10:50:06 +0530

    chore: Update Facebook SDK initialization in Facebook.jsx
    The Facebook.jsx component has been updated to improve the initialization of the Facebook SDK. The previous code has been commented out and replaced with a new script that loads the SDK asynchronously and includes the necessary environment variables. This change ensures that the Facebook SDK is initialized correctly and allows for better compatibility with the updated Facebook API version 'v8.0'.
    
    Please review and merge these changes as necessary.

--- src/Components/facebook/Facebook.jsx
+++ src/Components/facebook/Facebook.jsx
@@ -4,23 +4,23 @@ function FacebookLogin() {
     useEffect(() => {
         // Initialize the Facebook SDK
         console.log('Initializing Facebook SDK', process.env.REACT_APP_FACEBOOK_APP_ID)
-        window.fbAsyncInit = function() {
-            window.FB.init({
-                appId: process.env.REACT_APP_FACEBOOK_APP_ID, 
-                cookie: true,
-                xfbml: true,
-                version: 'v8.0'
-            });
+        // window.fbAsyncInit = function() {
+        //     window.FB.init({
+        //         appId: process.env.REACT_APP_FACEBOOK_APP_ID, 
+        //         cookie: true,
+        //         xfbml: true,
+        //         version: 'v8.0'
+        //     });
 
-            window.FB.AppEvents.logPageView();
-        };
+        //     window.FB.AppEvents.logPageView();
+        // };
 
         // Load the SDK asynchronously
         (function(d, s, id) {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) { return; }
             js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            js.src = `https://connect.facebook.net/en_US/sdk.js#version=v2.2&appId=${process.env.REACT_APP_FACEBOOK_APP_ID}&xfbml=true&autoLogAppEvents=true`;
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
     }, []);
